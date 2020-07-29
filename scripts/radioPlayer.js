c58cc1a3fd14e44c572c7dcd3b794dfa880f0c3d
Anna Volkova <va.personal@yandex.ru>
2020-07-29 15:30:48 +0300

    Programming audio-player

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,3 +1,61 @@
 export const radioPlayerInit = () => {
-    console.log('radio inited')
-}
+    const radio = document.querySelector('.radio');
+    const radioCoverImg = document.querySelector('.radio-cover__img');
+    const radioNavigation = document.querySelector('.radio-navigation');
+    const radioHeader = document.querySelector('.radio-header__big');
+    const radioItem = document.querySelectorAll('.radio-item');
+    const radioStop = document.querySelector('.radio-stop');
+
+    const audio = new Audio(); // объект со свойствами как у видеоплеера
+    audio.type = 'audio/aac';
+
+
+    radioStop.disabled = true;
+
+    const toggleIcon = () => {
+        if (audio.paused) {
+            radio.classList.remove('play');
+            radioStop.classList.remove('fa-stop');
+            radioStop.classList.add('fa-play');
+        } else {
+            radio.classList.add('play');
+            radioStop.classList.add('fa-stop');
+            radioStop.classList.remove('fa-play');
+        }
+    };
+
+    const selectStation = (station) => {
+        radioItem.forEach(item => item.classList.remove('select'));
+        station.classList.add('select');
+    };
+
+    let currentName;
+
+    radioNavigation.addEventListener('change', (evt) => {
+        const target = evt.target;
+        radioStop.disabled = false;
+
+        audio.src = target.dataset.radioStantion;
+
+        audio.play();
+        toggleIcon();
+
+        const parent = target.closest('.radio-item');
+        currentName = parent.textContent;
+        radioHeader.textContent = currentName;
+        selectStation(parent);
+
+        radioCoverImg.src = parent.querySelector('.radio-img').src;
+    });
+
+    radioStop.addEventListener('click', () => {
+       if (audio.paused) {
+           audio.play();
+           radioHeader.textContent = currentName;
+       } else {
+           audio.pause();
+           radioHeader.textContent = 'Пауза'
+       }
+        toggleIcon();
+    });
+};
--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,42 +1,39 @@
 export const videoPlayerInit = () => {
-    console.log('video inited');
-};
+    const videoPlayer = document.querySelector('.video-player');
+    const videoButtonPlay = document.querySelector('.video-button__play');
+    const videoButtonStop = document.querySelector('.video-button__stop');
+    const videoTimePassed = document.querySelector('.video-time__passed');
+    const videoProgress = document.querySelector('.video-progress');
+    const videoTimeTotal = document.querySelector('.video-time__total');
 
-const videoPlayer = document.querySelector('.video-player');
-const videoButtonPlay = document.querySelector('.video-button__play');
-const videoButtonStop = document.querySelector('.video-button__stop');
-const videoTimePassed = document.querySelector('.video-time__passed');
-const videoProgress = document.querySelector('.video-progress');
-const videoTimeTotal = document.querySelector('.video-time__total');
+    const toggleIcon = () => {
+        if (videoPlayer.paused) {
+            videoButtonPlay.classList.remove('fa-pause');
+            videoButtonPlay.classList.add('fa-play');
+        } else {
+            videoButtonPlay.classList.add('fa-pause');
+            videoButtonPlay.classList.remove('fa-play');
+        }
+    };
 
-const toggleIcon = () => {
-    if (videoPlayer.paused) {
-        videoButtonPlay.classList.remove('fa-pause');
-        videoButtonPlay.classList.add('fa-play');
-    } else {
-        videoButtonPlay.classList.add('fa-pause');
-        videoButtonPlay.classList.remove('fa-play');
-    }
-};
+    const togglePlay = () => {
+        if (videoPlayer.paused) {
+            videoPlayer.play();
+        } else {
+            videoPlayer.pause();
+        }
 
-const togglePlay = () => {
-    if (videoPlayer.paused) {
-        videoPlayer.play();
-    } else {
-        videoPlayer.pause();
-    }
-
-    toggleIcon();
-};
+        toggleIcon();
+    };
 
 
-videoPlayer.addEventListener('click', togglePlay);
-videoButtonPlay.addEventListener('click', togglePlay);
-document.addEventListener('keydown', (evt) => {
-    if (evt.code === 'Space') {
-        togglePlay();
-    }
-});
+    videoPlayer.addEventListener('click', togglePlay);
+    videoButtonPlay.addEventListener('click', togglePlay);
+    document.addEventListener('keydown', (evt) => {
+        if (evt.code === 'Space') {
+            togglePlay();
+        }
+    });
 
 
 // альтернативный способ запуска toggleIcon для любого типа плееров
@@ -44,50 +41,52 @@ document.addEventListener('keydown', (evt) => {
 // videoPlayer.addEventListener('play', toggleIcon);
 // videoPlayer.addEventListener('pause', toggleIcon);
 
-const stopPLay = () => {
-    videoPlayer.pause();
-    videoPlayer.currentTime = 0;
-    toggleIcon();
-};
+    const stopPLay = () => {
+        videoPlayer.pause();
+        videoPlayer.currentTime = 0;
+        toggleIcon();
+    };
 
-videoButtonStop.addEventListener('click', stopPLay);
+    videoButtonStop.addEventListener('click', stopPLay);
 
 // n - это переменная, обозначающая число
-const addZero = n => n < 10 ? '0' + n : n;
+    const addZero = n => n < 10 ? '0' + n : n;
 
-videoPlayer.addEventListener('timeupdate', () => {
-    const currentTime = videoPlayer.currentTime;
-    const duration = videoPlayer.duration;
+    videoPlayer.addEventListener('timeupdate', () => {
+        const currentTime = videoPlayer.currentTime;
+        const duration = videoPlayer.duration;
 
-    videoProgress.value = (currentTime / duration) * 100;
+        videoProgress.value = (currentTime / duration) * 100;
 
-    let minutesPassed = Math.floor(currentTime / 60);
-    let secondsPassed = Math.floor(currentTime % 60);
+        let minutesPassed = Math.floor(currentTime / 60);
+        let secondsPassed = Math.floor(currentTime % 60);
 
-    let minutesTotal = Math.floor(duration / 60);
-    let secondsTotal = Math.floor(duration % 60);
+        let minutesTotal = Math.floor(duration / 60);
+        let secondsTotal = Math.floor(duration % 60);
 
-    videoTimePassed.textContent = addZero(minutesPassed) + ':' + addZero(secondsPassed);
-    videoTimeTotal.textContent = addZero(minutesTotal) + ':' + addZero(secondsTotal);
-});
+        videoTimePassed.textContent = addZero(minutesPassed) + ':' + addZero(secondsPassed);
+        videoTimeTotal.textContent = addZero(minutesTotal) + ':' + addZero(secondsTotal);
+    });
 
-videoProgress.addEventListener('change', () => {
-    const duration = videoPlayer.duration;
-    const value = videoProgress.value;
+    videoProgress.addEventListener('change', () => {
+        const duration = videoPlayer.duration;
+        const value = videoProgress.value;
 
-    videoPlayer.currentTime = (value * duration) / 100;
-});
+        videoPlayer.currentTime = (value * duration) / 100;
+    });
 
 //fullscreen
-const videoFullScreen = document.querySelector('.video-fullscreen');
+    const videoFullScreen = document.querySelector('.video-fullscreen');
 
-videoFullScreen.addEventListener('click', () => {
-    videoPlayer.requestFullscreen();
-});
+    videoFullScreen.addEventListener('click', () => {
+        videoPlayer.requestFullscreen();
+    });
 
 // управление звуком
-const videoVolume = document.querySelector('.video-volume');
+    const videoVolume = document.querySelector('.video-volume');
 
-videoVolume.addEventListener('input', () => {
-    videoPlayer.volume = videoVolume.value / 100;
-});
+    videoVolume.addEventListener('input', (evt) => {
+        videoPlayer.volume = videoVolume.value / 100;
+    });
+};
+
